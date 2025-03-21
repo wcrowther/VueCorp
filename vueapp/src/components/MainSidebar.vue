51fc4920a343dd97c052e02bf869aa09476470f5
wcrowther <wcrowther22@gmail.com>
2025-03-21 01:31:23 -0400

    Stash alternate LoginView solutions.

--- vueapp/src/components/MainSidebar.vue
+++ vueapp/src/components/MainSidebar.vue
@@ -19,18 +19,17 @@
 </script>
 
 <template>
-
-    <!-- NOTE: 'v-show' NOT 'v-if' as we want to keep alive content and not have to recreate it  -->
     
-    <div class="absolute z-50 xs:relative flex-none transform transition-all duration-[300ms] overflow-hidden"
-        :class="[ sideBarHidden ? 'w-0' : 'w-full xs:w-[300px]']">
+    <div :class="['absolute z-50 xs:relative flex-none transform transition-all duration-[300ms] overflow-hidden',
+        sideBarHidden ? 'w-0' : 'w-full xs:w-[300px]']">
+
         <div class="absolute right-0 w-full min-w-[300px] xs:relative xs:w-[300px] xs:min-w-1">
             <slot></slot>
         </div> 
+
     </div>
 
 </template>
-
 
 <!-- 
     <div class="bg-red h-screen">FAKE CONTENT</div> 
