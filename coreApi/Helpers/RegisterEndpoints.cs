3b76c61439453d75d8e375f5a7d502e2adcbfe07
wcrowther <wcrowther22@gmail.com>
2025-04-21 22:38:51 -0400

    RegisterEndpoints minor update.

--- coreApi/Helpers/RegisterEndpoints.cs
+++ coreApi/Helpers/RegisterEndpoints.cs
@@ -6,18 +6,21 @@ public static class EndpointHelper
 {
 	// Add an EndPoints static partial named for the MapGroup and it will be 
 	// registered by this helper. See existing examples in the Endpoints folder.
+	// 'No action descriptors found...' warning can be ignored as it is for Controller routes.
 
 	public static void RegisterMyEndpoints(this WebApplication app)
 	{
 		var staticMethods = typeof(Endpoints).GetMethods().Where(a => a.IsStatic && a.IsPublic);
 
+		// For logging: var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
 		foreach (var method in staticMethods)
 		{
+			// logger.LogInformation($"Adding Route Endpoint {method.Name}");
+
 			Debug.WriteLine($"Adding Endpoint {method.Name}");
+
 			method.Invoke(null, [app]);
 		}
 	}
 }
-
-
-// One liner version -> staticMethods.ForEach(a => a.Invoke(null, [app]));
