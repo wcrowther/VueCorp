a591e19a4b0a208653f7212507600b86e583a5aa
wcrowther <wcrowther22@gmail.com>
2025-04-24 15:31:51 -0400

    Updated Returns.cs

--- coreLogic/Models/Generic/Returns.cs
+++ coreLogic/Models/Generic/Returns.cs
@@ -17,6 +17,9 @@ public class Returns<T>(T data = default, Error error = null)
 	public static Returns<T> Result(T data, string errorMessage = "No data returned.")
 		=> data is not null ? new(data) : Failure(errorMessage);
 
+	public static Returns<T> Result(T data, Func<T, bool> func, string errorMessage)
+		=> func(data) ? new(data) : Failure(errorMessage);
+
 	public static Returns<T> Failure(string errorMessage)
 		=> new(error: new Error(errorMessage));
 
@@ -24,12 +27,35 @@ public class Returns<T>(T data = default, Error error = null)
 
 	public static Returns<T> Failure(Exception exception) => new(error: exception);
 
-
-	public static implicit operator Returns<T>(T data)				=> Success(data);
-
-	public static implicit operator Returns<T>(Error error)			=> Failure(error);
+	public static implicit operator Returns<T>(Error error) => Failure(error);
 
 	public static implicit operator Returns<T>(Exception exception) => Failure(exception);
 }
 
-public class Returns(string data = "", Error error = null) : Returns<string>(data, error);
+public class Returns(string data = null, Error error = null) : Returns<string>(data, error)
+{
+	public static new Returns Success(string data)
+		=> new(data);
+
+	public static new Returns Result(string data, string errorMessage = "No data returned.")
+		=> data is not null ? new(data) : Failure(errorMessage);
+
+	public static new Returns Result(string data, Func<string, bool> func, string errorMessage)
+		=> func(data) ? new(data) : Failure(errorMessage);
+
+	public static new Returns Failure(string errorMessage)
+		=> new(error: new Error(errorMessage));
+
+	public static new Returns Failure(Error error)
+		=> new(error: error);
+
+	public static new Returns Failure(Exception exception)
+		=> new(error: exception);
+
+
+	public static implicit operator Returns(Error error) => Failure(error);
+
+	public static implicit operator Returns(Exception exception) => Failure(exception);
+
+	public override string ToString() => Ok ? Data.ToString() ?? "" : Error.Message;
+}
